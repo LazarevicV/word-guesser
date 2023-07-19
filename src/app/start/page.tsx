0fcbe30f00c34bfe4745a3697fcbf1963d593339
LazarevicV <vukadinlazarevic@gmail.com>
2023-07-19 17:06:37 +0200

    welcome page

---
+++ src/app/start/page.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="mt-8">
+        <h2 className="text-4xl font-bold">Instructions:</h2>
+        <ul className="mt-4 text-xl">
+          <li>Guess the correct word by entering letters.</li>
+          <li>You have a limited number of attempts.</li>
+          <li>Choose the difficulty level below:</li>
+        </ul>
+        <div className="mt-4 flex space-x-4">
+          <a
+            href="/game?difficulty=easy"
+            className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded"
+          >
+            Easy
+          </a>
+          <a
+            href="/game?difficulty=medium"
+            className="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded"
+          >
+            Medium
+          </a>
+          <a
+            href="/game?difficulty=hard"
+            className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded"
+          >
+            Hard
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
